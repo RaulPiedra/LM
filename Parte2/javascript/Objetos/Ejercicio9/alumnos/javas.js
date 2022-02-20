62fbb3036e4152dfcc6001ab573961c7dae80d93
RaulPiedra <terfezio@gmail.com>
2022-02-20 21:37:54 +0100

    ssfg

--- Parte2/javascript/Objetos/Ejercicio9/alumnos/javas.js
+++ Parte2/javascript/Objetos/Ejercicio9/alumnos/javas.js
@@ -3,30 +3,55 @@ var v_boton_mates = document.getElementById("bma");
 var v_boton_ingles = document.getElementById("bi");
 var v_boton_fisica = document.getElementById("bf");
 var v_boton_musica = document.getElementById("bm");
+var v_boton_reset = document.getElementById("reset");
 
-var color = "red";
- v_boton_lengua.addEventListener("click",muestra(color));
- v_boton_mates.addEventListener("click", muestra);
- v_boton_ingles.addEventListener("click", muestra);
- v_boton_fisica.addEventListener("click", muestra);
- v_boton_musica.addEventListener("click", muestra);
+var leng = document.getElementsByClassName("lengua");
+var mat = document.getElementsByClassName("mates");
+var ing = document.getElementsByClassName("ingles");
+var fis = document.getElementsByClassName("fisica");
+var mus = document.getElementsByClassName("musica");
 
- 
-  var leng = document.getElementsByClassName("lengua");
-  var mat = document.getElementsByClassName("mates");
-  var ing = document.getElementsByClassName("ingles");
-  var fis = document.getElementsByClassName("fisica");
-  var mus = document.getElementsByClassName("musica");
-  
-  function muestra(event) {
+v_boton_lengua.addEventListener("click",muestraLengua);
+v_boton_mates.addEventListener("click", muestraMates);
+v_boton_ingles.addEventListener("click", muestraIngles);
+v_boton_fisica.addEventListener("click", muestraFisica);
+v_boton_musica.addEventListener("click", muestraMusica);
+v_boton_reset.addEventListener("click", reset);
+
+var filas = document.getElementsByTagName("td");
+var botones = document.getElementsByTagName("input");
+function reset() {
+      for (var i = 0; i < filas.length; i++) {
+            filas[i].style.backgroundColor = "white";
             
-        for (var i = 0; i < leng.length; i++) {
-            leng[i].style.backgroundColor=color;
-      }  
-    }
+      }
+      for (var j = 0; j < botones.length; j++) {
+            botones[j].style.backgroundColor = "lightgray";
+      }
+}
 
-  function resalta(array, color){
-        for (var i = 0; i < array.length; i++) {
-              array[i].style.backgroundColor=color;
-        }     
-  }
+function muestraLengua() {
+      for (var i = 0; i < leng.length; i++) {
+            leng[i].style.backgroundColor = "red";
+      }
+}
+function muestraMates() {
+      for (var i = 0; i < mat.length; i++) {
+            mat[i].style.backgroundColor = "blue";
+      }
+}
+function muestraIngles() {
+      for (var i = 0; i < ing.length; i++) {
+            ing[i].style.backgroundColor = "orange";
+      }
+}
+function muestraFisica() {
+      for (var i = 0; i < fis.length; i++) {
+            fis[i].style.backgroundColor = "gray";
+      }
+}
+function muestraMusica() {
+      for (var i = 0; i < mus.length; i++) {
+            mus[i].style.backgroundColor = "green";
+      }
+}
