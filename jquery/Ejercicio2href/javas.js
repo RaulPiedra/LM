ac3aa418ec17b6c5a50c641665ff88400dc34b24
RaulPiedra <terfezio@gmail.com>
2022-03-09 19:06:45 +0100

    nnml

---
+++ jquery/Ejercicio2href/javas.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("input").css("backgroundColor", "#dfe5dd");
+    $("input").css("border", "3px solid red");
+    $("input").css("color", "blue").css("borderRadius", "20px");
+    $("#faldoncillo").css("textAlign", "center").css("font-size", "2em").css("color", "white");
+    $("#faldoncillo").append("Las mejores playas de Almeria");
+});
